f4ca45ae004c6f3976214d1cc25afd484ac5c82a
Natoile <anatole.surel@laposte.net>
2023-01-04 20:48:03 +0100

    Update model.py

--- src/nn/models/model.py
+++ src/nn/models/model.py
@@ -4,18 +4,23 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
+
         self.fc = nn.Sequential(
             nn.Linear(54, 128),
             nn.ReLU(),
-            nn.Linear(128, 128),
+            nn.Linear(128, 256),
             nn.ReLU(),
-            nn.Linear(128, 128),
+            nn.Linear(256, 512),
             nn.ReLU(),
-            nn.Linear(128, 128),
+            nn.Dropout(0.2),
+            nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(128, 128),
+            nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(128, 128),
+            nn.Dropout(0.2),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
             nn.ReLU(),
             nn.Linear(128, 128),
             nn.ReLU(),
@@ -24,4 +29,5 @@ class Net(nn.Module):
 
     def forward(self, x):
         x = self.fc(x)
-        return x
+        output = F.log_softmax(x, dim=1)
+        return output
