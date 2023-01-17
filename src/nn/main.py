5ea56f4d433a20ce9a3218f75e7024555a516c28
Natoile <94048850+Natoile@users.noreply.github.com>
2023-01-17 20:10:59 +0100

    Add files via upload

--- src/nn/main.py
+++ src/nn/main.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import argparse
 import glob
 import os
+import random
 
 import numpy as np
 import torch
@@ -14,6 +15,10 @@ from sklearn.model_selection import train_test_split
 from preprocess.custom_dataset import CustomDataset
 from trainer import Trainer
 from models.model import Net
+
+random.seed(0)
+np.random.seed(0)
+torch.manual_seed(0)
 
 #------------- Ce programme vise a resoudre le rubik s cube via un reseau de neuronne-------------------
 
@@ -28,14 +33,14 @@ def split():
     dataset = pd.concat(li, axis=0, ignore_index=True)
     dataset = dataset.drop_duplicates()
 
-    train, test = train_test_split(dataset, test_size=0.2)
+    train, test = train_test_split(dataset, test_size=0.2, shuffle=True)
     print(f"Train set: size = {len(train)}")
     print(f"Test set: size = {len(test)}")
     return train, test
 
 
 def main():
-    num_epochs = 10
+    num_epochs = 40
     
     #configuration Pytorch (utilisation GPU si possible)
     use_cuda = torch.cuda.is_available()
@@ -44,12 +49,12 @@ def main():
     if use_cuda:
         device = torch.device("cuda")
     else:
-        device = torch.device("cpu")   
+        device = torch.device("cpu") 
     print(f"Using device: {device}")
     
     
     #Configuration Data / Preprocessing
-    train_kwargs = {'batch_size': 64}
+    train_kwargs = {'batch_size': 128}
     test_kwargs = {'batch_size': 4096}
     
     if use_cuda:
@@ -59,39 +64,46 @@ def main():
         
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
- 
+        
+    load_from_checkpoint='../../models/nn2.pt'
 
-    dataset1, dataset2 = split()
-    
-    
-    train_loader = torch.utils.data.DataLoader(
-        CustomDataset(dataset1),
-        **train_kwargs
-    )
-    test_loader = torch.utils.data.DataLoader(
-        CustomDataset(dataset2),
-        **test_kwargs
-    )
-
-    
     model = Net().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    optimizer = optim.Adam(model.parameters(), lr=2e-4)
     scheduler = StepLR(optimizer, step_size=1, gamma=1)
-    trainer = Trainer(model, optimizer, scheduler, num_epochs, device)
-
+    trainer = Trainer(
+        model,
+        optimizer,
+        scheduler,
+        num_epochs,
+        device,
+        load_from_checkpoint='../../models/nn2.pt'
+    )
     
-    #Training
-    trainer.fit(train_loader, test_loader)
-    torch.save(model.state_dict(), '../../models/res.pt')
+    
+    if os.path.exists(load_from_checkpoint) == False :
+        prin("daccord")
+        dataset1, dataset2 = split()
 
+        train_loader = torch.utils.data.DataLoader(
+            CustomDataset(dataset1, predict_or_train = 0),
+            **train_kwargs
+        )
+        test_loader = torch.utils.data.DataLoader(
+            CustomDataset(dataset2,  predict_or_train = 0),
+            **test_kwargs
+        )
 
+        #Training
+        trainer.fit(train_loader, test_loader)
+
+        
+        
     #Prediction
-    rubik_str = pd.read_csv('../../data/Creation_Data/prediction.csv')
-    customprediction = CustomDataset(rubik_str)
-    rubik_str = customprediction.preprocess(rubik_str)
-    
+    rubik_str = pd.read_csv('../../data/generated_data/prediction.csv')
+    customprediction = CustomDataset(rubik_str,  predict_or_train = 1)
+
     predict_loader = torch.utils.data.DataLoader(
-        CustomDataset([rubik_str], train=False),
+        CustomDataset(rubik_str,  predict_or_train = 1),
         **test_kwargs
     )
     trainer.predict(predict_loader)
