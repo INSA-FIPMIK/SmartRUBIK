b88c952c6ceb03de7297c59ac8cf11a02be57703
Natoile <94048850+Natoile@users.noreply.github.com>
2023-01-17 20:12:29 +0100

    Add files via upload

--- src/nn/models/model.py
+++ src/nn/models/model.py
@@ -1,33 +1,36 @@
+import time
+
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils.rnn import pad_sequence
+from transformers import BartConfig, BartForConditionalGeneration
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
+        self.config = BartConfig()
+        self.config.max_position_embeddings = 256
+        self.config.vocab_size = 27
+        self.config.encoder_ffn_dim = 512
+        self.config.encoder_layers = 6
+        self.config.decoder_ffn_dim = 512
+        self.config.decoder_layers = 6
+        self.config.pad_token_id = 26
+        self.config.eos_token_id = 25
+        self.config.bos_token_id = 24
+        self.config.d_model = 256
+        self.tf = BartForConditionalGeneration(self.config)
+        #print(self.tf)
+        def count_parameters(model):
+            return sum(p.numel() for p in model.parameters() if p.requires_grad)
+        #print(f"Number of trainable parameters: {count_parameters(self.tf)}")
 
-        self.fc = nn.Sequential(
-            nn.Linear(54, 128),
-            nn.ReLU(),
-            nn.Linear(128, 256),
-            nn.ReLU(),
-            nn.Linear(256, 512),
-            nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, 128),
-            nn.ReLU(),
-            nn.Linear(128, 18),
+    def forward(self, x, y=None):
+        attention_mask = x != self.config.pad_token_id
+        outputs = self.tf(
+            input_ids=x,
+            attention_mask=attention_mask,
+            labels=y
         )
-
-    def forward(self, x):
-        x = self.fc(x)
-        output = F.log_softmax(x, dim=1)
-        return output
+        return outputs
