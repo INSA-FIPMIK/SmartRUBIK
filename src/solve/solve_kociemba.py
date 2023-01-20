f778e3cf9df69f109180fe78c52bff41151b6b72
Your Name <you@example.com>
2023-01-20 23:50:39 +0000

    Essai nouveau programme pour journee porte ouvertes

---
+++ src/solve/solve_kociemba.py
@@ -0,0 +1,54 @@
+import kociemba
+
+
+def solve_kociemba():
+    cube = label_col['text']
+    cube = cube.replace('Colors : ',"")
+    cube=cube.replace('w','U')
+    cube=cube.replace('b','R')
+    cube=cube.replace('r','F')
+    cube=cube.replace('y','D')
+    cube=cube.replace('g','L')
+    cube=cube.replace('o','B')
+
+    sol=kociemba.solve(cube)
+    print("La solution est:")
+    print(sol)
+
+    sol=sol.replace("R2","RR")
+    sol=sol.replace("L2","LL")
+    sol=sol.replace("D2","DD")
+    sol=sol.replace("B2","BB")
+    sol=sol.replace("F2","FF")
+    sol=sol.replace("U2","UU")
+
+    sol=sol.replace("R'","H")
+    sol=sol.replace("R","r")
+    sol=sol.replace("H","R")
+
+    sol=sol.replace("L'","l")
+
+    sol=sol.replace("U'","u")
+
+    sol=sol.replace("D'","H")
+    sol=sol.replace("D","d")
+    sol=sol.replace("H","D")
+
+    sol=sol.replace("F'","H")
+    sol=sol.replace("F","f")
+    sol=sol.replace("H","F")
+
+    sol=sol.replace("B'","b")
+
+    sol=sol.replace(" ","")
+    sol = '&'+sol+'#'
+
+    print(sol)
+    label_sol.config(text='Solution : ' + sol)
+
+    # Serial communication
+    ser.write(sol.encode('ascii'))
+
+    # Disable the solve button
+    button_solve.config(state="disable")
+    return
